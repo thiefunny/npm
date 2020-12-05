28013beef60729afb594bf10f5c1a44a234c9aac
thiefunny <mikolaj@walanus.com>
2020-12-05 23:19:40 +0100

    v 0.5

--- source/js/app.js
+++ source/js/app.js
@@ -1,43 +1,95 @@
-let x = 0;
 let aFreq = 440;
 let eFreq = aFreq * 1.5;
 let dFreq = aFreq / 1.5;
 let gFreq = aFreq / 1.5 / 1.5;
 let freqArr = [eFreq, aFreq, dFreq, gFreq];
+let freqChange = 2;
+
 let indexPlaying;
+let indexCurrent;
 
 const mainEl = document.querySelector("main");
+const upEl = document.querySelector(".up");
+const downEl = document.querySelector(".down");
+const resetEl = document.querySelector(".reset");
+const infoEl = document.querySelector(".info");
 
-const context = new AudioContext();
-const oscillator = context.createOscillator();
+infoEl.innerHTML = aFreq;
+
+const ctx = new AudioContext();
+const oscillator = ctx.createOscillator();
 oscillator.type = "triangle";
-const gainNode = context.createGain();
-oscillator.start(0);
+const gainNode = ctx.createGain();
 oscillator.connect(gainNode);
-oscillator.disconnect(gainNode);
-gainNode.connect(context.destination);
+oscillator.start(0);
+gainNode.gain.linearRampToValueAtTime(0, ctx.currentTime);
+gainNode.connect(ctx.destination);
 
-// const duration = 2;
-
-const play = index => {
-    oscillator.frequency.value = index;
-    oscillator.connect(gainNode);
+const on = _ => {
+    gainNode.gain.linearRampToValueAtTime(1, ctx.currentTime);
+}
+const off = _ => {
+    gainNode.gain.linearRampToValueAtTime(0, ctx.currentTime);
 }
 
 const action = _ => {
     mainEl.addEventListener("click", event => {
         const list = [...event.target.parentNode.children];
-        let index = list.indexOf(event.target);
+        indexCurrent = list.indexOf(event.target);
 
-        if (indexPlaying === index) {
-            oscillator.disconnect(gainNode);
+        if (indexPlaying === indexCurrent) {
+            off();
+            event.target.classList.remove("active");
             indexPlaying = null;
         } else {
-            play(freqArr[index]);
-            indexPlaying = index;
+            oscillator.frequency.value = freqArr[indexCurrent];
+            on();
+            for (let elem of list) {
+                elem.classList.remove("active");
+            }
+
+            event.target.classList.add("active");
+            indexPlaying = indexCurrent;
         }
 
     })
 }
 
+const tune = _ => {
+    upEl.addEventListener("click", _ => {
+        aFreq += freqChange;
+        eFreq = aFreq * 1.5;
+        dFreq = aFreq / 1.5;
+        gFreq = aFreq / 1.5 / 1.5;
+        freqArr = [eFreq, aFreq, dFreq, gFreq];
+        infoEl.innerHTML = aFreq;
+
+        oscillator.frequency.value = freqArr[indexCurrent];
+    })
+    downEl.addEventListener("click", _ => {
+        aFreq -= freqChange;
+        eFreq = aFreq * 1.5;
+        dFreq = aFreq / 1.5;
+        gFreq = aFreq / 1.5 / 1.5;
+        freqArr = [eFreq, aFreq, dFreq, gFreq];
+        infoEl.innerHTML = aFreq;
+
+        oscillator.frequency.value = freqArr[indexCurrent];
+
+    })
+    resetEl.addEventListener("click", _ => {
+        aFreq = 440;
+        eFreq = aFreq * 1.5;
+        dFreq = aFreq / 1.5;
+        gFreq = aFreq / 1.5 / 1.5;
+        freqArr = [eFreq, aFreq, dFreq, gFreq];
+        infoEl.innerHTML = aFreq;
+
+        oscillator.frequency.value = freqArr[indexCurrent];
+
+    })
+
+}
+
 action();
+tune();
