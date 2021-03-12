8a06997c36b9d593008bd26f3e511080ced09f66
thiefunny <mikolaj@walans.com>
2021-03-12 15:01:50 +0100

    work in progress [SVG]

--- source/js/anim9stackoverflow.js
+++ source/js/anim9stackoverflow.js
@@ -2,16 +2,13 @@ const circle = document.querySelector("circle")
 
 window.addEventListener("click", mouse => {
 
-    let targetCircleX = mouse.clientX;
-    let targetCircleY = mouse.clientY;
-
     const animation = _ => {
 
         let getCx = Number(circle.getAttribute('cx'))
         let getCy = Number(circle.getAttribute('cy'))
 
-        circle.setAttribute("cx", `${getCx + (targetCircleX - getCx)/10}`);
-        circle.setAttribute("cy", `${getCy + (targetCircleY - getCy)/10}`);
+        circle.setAttribute("cx", `${getCx + (mouse.clientX - getCx)/10}`);
+        circle.setAttribute("cy", `${getCy + (mouse.clientY - getCy)/10}`);
 
         requestAnimationFrame(animation)
 
