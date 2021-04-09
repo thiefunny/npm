f2868efb44e69362fcdcf279c4aa678c32d8f0c4
thiefunny <mikolaj@walans.com>
2021-04-09 12:00:04 +0200

    work in progress - spirals many options

---
+++ spirals/spirals4.js
@@ -0,0 +1,92 @@
+const canvasEl = document.querySelector("#drawing");
+const ctx = canvasEl.getContext('2d');
+const canvasWidth = window.innerWidth / 1.1;
+const canvasHeight = window.innerHeight / 1.3;
+canvasEl.setAttribute("width", canvasWidth + "px");
+canvasEl.setAttribute("height", canvasHeight + "px");
+
+
+const speed = 10;
+
+function circle(x, y, r, hue, saturation, lightness, alpha) {
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'hsla(' + hue + ', ' + saturation + '%, ' + lightness + '%, ' + alpha + ')';
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+
+}
+
+const drawing = _ => {
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    // const quantity = Math.floor(Math.random() * 100);
+    const quantity = 100;
+    let spiralsArr = new Array(quantity);
+    let frame = 0;
+    const hueChoice = Math.floor(Math.random() * 50+180);
+
+
+    for (let i = 0; i < quantity; i++) {
+        // let x = canvasWidth * Math.random(),
+        let x = canvasWidth /2 ,
+            y = canvasHeight /2,
+            r = Math.random() * 20,
+            alpha = Math.random() * 0.5,
+            hue = Math.floor(Math.random() * 80) + hueChoice,
+            saturation = 100,
+            lightness = 30,
+            rstep = Math.random() * 0.3,
+            rdiv = Math.random() * 5 + 5,
+            r2 = Math.random() * 5,
+            r3 = Math.random() * 2 + 0.5,
+            direction = Math.round(Math.random());
+
+        spiralsArr[i] = {
+            x,
+            y,
+            r,
+            alpha,
+            hue,
+            saturation,
+            lightness,
+            rstep,
+            rdiv,
+            r2,
+            r3,
+            direction
+        }
+    }
+
+    const draw = setInterval(function () {
+
+        if (frame < 700) {
+
+            spiralsArr.forEach(elem => {
+                elem.r2 += Number(elem.r3);
+
+                if (elem.direction) {
+                    elem.r += Number(elem.rstep);
+                    elem.hue = elem.hue * 1.001;
+                } else {
+                    elem.r -= Number(elem.rstep);
+                    elem.hue = elem.hue * 0.999;
+                }
+
+                elem.x += elem.r / Number(elem.rdiv) * Math.sin(elem.r) * Math.sin(elem.r);
+                elem.y += elem.r / Number(elem.rdiv)  * Math.cos(elem.r) * Math.cos(elem.r);
+                
+
+                circle(elem.x, elem.y, elem.r2, elem.hue, elem.saturation, elem.lightness, elem.alpha);
+            })
+            frame++
+        } else {
+            clearInterval(draw);
+            // drawing()
+        }
+    }, speed);
+}
+
+drawing()
---
+++ spirals/spirals7.js
@@ -0,0 +1,98 @@
+const canvasEl = document.querySelector("#drawing");
+const ctx = canvasEl.getContext('2d');
+const canvasWidth = window.innerWidth / 1.1;
+const canvasHeight = window.innerHeight / 1.3;
+canvasEl.setAttribute("width", canvasWidth + "px");
+canvasEl.setAttribute("height", canvasHeight + "px");
+const canvasCenterX = Math.floor(canvasWidth / 2);
+const canvasCenterY = Math.floor(canvasHeight / 2);
+
+const speed = 10;
+
+function circle(x, y, r, hue, saturation, lightness, alpha) {
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'hsla(' + hue + ', ' + saturation + '%, ' + lightness + '%, ' + alpha + ')';
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+
+}
+
+const drawing = _ => {
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    const quantity = 1;
+    let spiralsArr = new Array(quantity);
+    let frame = 0;
+    const hueChoice = Math.floor(Math.random() * 255);
+
+
+    for (let i = 0; i < quantity; i++) {
+        let x = -10,
+            y = 0,
+            r = Math.random() * 20,
+            // alpha = Math.random() * 0.33,
+            alpha = 1,
+            hue = Math.floor(Math.random() * 80) + hueChoice,
+            saturation = 100,
+            lightness = 30,
+            rstep = Math.random() * 0.3,
+            rdiv = Math.random() * 5 + 5,
+            r2 = 0.5,
+            r3 = Math.random() * 2 + 0.5,
+            direction = Math.round(Math.random());
+
+        spiralsArr[i] = {
+            x,
+            y,
+            r,
+            alpha,
+            hue,
+            saturation,
+            lightness,
+            rstep,
+            rdiv,
+            r2,
+            r3,
+            direction
+        }
+    }
+
+    // const draw = _ => {
+
+    for (let i = 0; i < 5000; i++) {
+
+        // if (frame < 100) {
+
+        spiralsArr.forEach(elem => {
+
+            // if (elem.direction) {
+            //     elem.r += Number(elem.rstep);
+            //     elem.hue = elem.hue * 1.0005;
+            // } else {
+            //     elem.r -= Number(elem.rstep);
+            //     elem.hue = elem.hue * 0.9995;
+            // }
+
+            elem.x = elem.x + 0.04
+            elem.y = 0;
+            elem.r2 = Math.abs(Math.sin(elem.x))*1.5;
+            // elem.r2 = 1;
+            elem.alpha = Math.abs(Math.sin(elem.x));
+            // console.log('miki')
+            circle(elem.x * 100 + canvasCenterX, -elem.y * 100 + canvasCenterY, elem.r2, elem.hue, elem.saturation, elem.lightness, elem.alpha);
+            // circle(elem.x*100  + canvasCenterX, -elem.y*100 + canvasCenterY, elem.r2, elem.hue, elem.saturation, elem.lightness, elem.alpha);
+        })
+
+    }
+    // frame++;
+    // } 
+    // requestAnimationFrame(draw)      
+    // };
+    // requestAnimationFrame(draw)
+    // draw()
+}
+
+drawing()
